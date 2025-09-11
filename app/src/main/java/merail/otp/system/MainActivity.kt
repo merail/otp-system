2560248dddee6af799066d971e951142cadac469
merail <r.mesherov@blanc.ru>
2025-09-11 21:56:47 +0300

    Fix status bar icon colors

--- app/src/main/java/merail/otp/system/MainActivity.kt
+++ app/src/main/java/merail/otp/system/MainActivity.kt
@@ -1,7 +1,9 @@
 package merail.otp.system
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,7 +15,10 @@ import androidx.compose.ui.Modifier
 internal class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+        )
         setContent {
             MaterialTheme {
                 Surface(
